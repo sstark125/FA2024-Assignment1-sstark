2613df58a5b5273c1bc91399477a78fdd2f5e4c7
sstark1 <sstark1@LB-CARES-208>
2024-09-05 14:01:58 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+namespace FA2024_Assignment1_sstark
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.Run();
+        }
+    }
+}
